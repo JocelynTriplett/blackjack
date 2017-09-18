978cf33cc7c9196a36c734ab89b74b4cd80227d6
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-09-18 13:24:51 -0400

    totals non-number cards

--- starter_files/lib/blackjack.rb
+++ starter_files/lib/blackjack.rb
@@ -22,6 +22,7 @@ class Blackjack
   def initialize(games: 1)
     @hand = Hand.new
     @games = games
+    @cash = 100
   end
 
 
@@ -31,16 +32,80 @@ def run
   end
 end
 
+def total_cards (card1, card2, total)
+  if (card1 === :J) ||
+     (card1 === :Q) ||
+     (card1 === :K)
+     card1 = 10
+
+  elsif (card1 === :A) && (total < 11)
+    card1 = 11
+  elsif (card1 === :A) && (total > 10)
+    card1 = 1
+  end
+  if (card2 === :J) ||
+     (card2 === :Q) ||
+     (card2 === :K)
+     card2 = 10
+  elsif (card2 === :A) && (total < 11)
+    card2 = 11
+  elsif (card2 === :A) && (total > 10)
+    card1 = 1
+  end
+  card1 + card2
+end
+
+
 def run_game
   puts @hand
   @hand.deal
   player_card_1 = @hand.player_cards[0].rank
   player_card_2 = @hand.player_cards[1].rank
-  total = player_card_1.to_i + player_card_2.to_i
+# TODO - add logic to total cards that aren't numbers
+  # total = player_card_1.to_i + player_card_2.to_i
+  total = total_cards(player_card_1, player_card_2, 0)
 
 
-  # puts "Hello and welcome to the game of blackjack! Let's begin."
+puts "Hello and welcome to the game of blackjack! Let's begin."
+# puts "You have $#{cash} and bet $10."
 puts "You have a #{player_card_1} and a #{player_card_2} in your hand. Your total is #{total}."
+# Do you want to (h)it or (s)tand? H
+#
+# You hit. You now have a 9, 8, and 4 in your hand. Your total is 19.
+# Do you want to (h)it or (s)tand? S
+#
+# You stand. Your total is 19.
+#
+# The dealer hits.
+# The dealer stands. The dealer has a total of 20. You lose!
+#
+# ---
+#
+# You have $90 and bet $10.
+# You have an A and a 7 in your hand. Your total is 18.
+# Do you want to (h)it or (s)tand? H
+#
+# You hit. You now have an A, 7, and 9 in your hand. Your total is 17.
+# Do you want to (h)it or (s)tand? H
+#
+# You hit. You now have an A, 7, 9, and 5 in your hand. Your total is 22.
+# You bust!
+#
+# ---
+#
+# You have $80 and bet $10.
+# You have a 5 and a 5 in your hand. Your total is 10.
+# Do you want to (h)it or (s)tand? H
+#
+# You hit. You now have a 5, 5, and A in your hand. Your total is 21.
+#
+# The dealer stands. The dealer has a total of 19. You win!
+#
+# ---
+#
+# You have $90 and bet $10.
+# ...
+# ```
   # @players.shuffle!
   #
   # @players.each do |player|
