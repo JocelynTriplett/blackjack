42086f1f03293ba301f79c68598a4e6264b0af21
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-09-19 17:01:39 -0400

    clean out some commented-out code

--- starter_files/lib/blackjack.rb
+++ starter_files/lib/blackjack.rb
@@ -119,100 +119,16 @@ end
 def run_game
 # Begin game
 puts "Hello and welcome to the game of blackjack! Let's begin."
+
   @hand = Hand.new
   @hand.deal
   make_bet
   report_values
 
-  # Begin hit_or_stand loops
-
   if hit_or_stand === true
     player_hits
   end
-#
-# You hit. You now have a 9, 8, and 4 in your hand. Your total is 19.
-# Do you want to (h)it or (s)tand? S
-#
-# You stand. Your total is 19.
-#
-# The dealer hits.
-# The dealer stands. The dealer has a total of 20. You lose!
-#
-# ---
-#
-# You have $90 and bet $10.
-# You have an A and a 7 in your hand. Your total is 18.
-# Do you want to (h)it or (s)tand? H
-#
-# You hit. You now have an A, 7, and 9 in your hand. Your total is 17.
-# Do you want to (h)it or (s)tand? H
-#
-# You hit. You now have an A, 7, 9, and 5 in your hand. Your total is 22.
-# You bust!
-#
-# ---
-#
-# You have $80 and bet $10.
-# You have a 5 and a 5 in your hand. Your total is 10.
-# Do you want to (h)it or (s)tand? H
-#
-# You hit. You now have a 5, 5, and A in your hand. Your total is 21.
-#
-# The dealer stands. The dealer has a total of 19. You win!
-#
-# ---
-#
-# You have $90 and bet $10.
-# ...
-# ```
-  # @players.shuffle!
-  #
-  # @players.each do |player|
-  #   player.start_game
-  # end
-  #
-  # @players.cycle.each do |player|
-  #
-  #   player.start_turn
-  #
-  #   while player.roll_again?
-  #     roll = roll_die
-  #     if roll == 1
-  #       log(player, "rolls a 1 and loses their turn.")
-  #     else
-  #       log(player, "rolls a #{roll}.")
-  #     end
-  #     player.record_roll(roll)
-  #
-  #   end
-  #
-  #   log(player, "ends their turn with a score of #{player.score}.")
-  #   player.end_turn
-  #
-  #   if player.score >= 100
-  #     log(player, "has won!")
-  #     break
-  #   end
-  # end
-  #
-  # @players.each do |player|
-  #   player.end_game
-  # end
 end
 end
 
 Blackjack.new.run
-
-  # def log(player, message)
-  #   puts "#{player.name} #{message}" unless @silent
-  # end
-  #
-  # def roll_die
-  #   rand(6) + 1
-  # end
-  #
-  #
-  #   @players.each do |player|
-  #     puts "#{player.name} won #{player.wins} game(s) and lost #{player.losses} game(s)."
-  #   end
-  # end
