40cbb769635b306ed4159b63ac32db32b7ab458f
Jocelyn Triplett <jocelyn.triplett@gmail.com>
2017-09-24 19:51:40 -0400

    move card totaling functions to hand class

--- starter_files/bin/blackjack.rb
+++ starter_files/bin/blackjack.rb
@@ -14,165 +14,161 @@ class Blackjack
     @games_won = 0
   end
 
-def run
-  begin_game
-end
+  def run
+    begin_game
+  end
 
-def hit_or_stand
-  puts "Do you want to (h)it or (s)tand?"
-  while true
-    print "Please enter (h)it or (s)tand: "
-    answer = gets.chomp.downcase
-    if answer[0] == "h"
-      player_hits
-      return false
-    elsif answer[0] == "s"
-      player_stands
-      return false
+  def speak_slower
+    sleep(1)
+  end
+
+  def hit_or_stand
+    puts "Do you want to (h)it or (s)tand?"
+    while true
+      speak_slower
+      print "Please enter (h)it or (s)tand: "
+      answer = gets.chomp.downcase
+      if answer[0] == "h"
+        player_hits
+        return false
+      elsif answer[0] == "s"
+        player_stands
+        return false
+      end
+      puts "That is not a valid answer!"
     end
-    puts "That is not a valid answer!"
   end
-end
-def add_ace
-  if @card_values.inject(&:+) < 11
-    @card_values.inject(&:+) + 11
-  else
-    @card_values.inject(&:+) + 1
-  end
-end
 
-def get_total(cards)
-  @card_values = cards.map { |card| card.rank  }
-  @card_values.map! { |value| (value === :J || value === :Q || value === :K ? 10 : value) }
-# if the hand includes an Ace
-  if @card_values.include?(:A)
-    @card_values.delete_at(@card_values.find_index(:A))
-# if the hand includes another Ace
-    if @card_values.include?(:A)
-      @card_values.delete_at(@card_values.find_index(:A))
-      @card_values.push(1)
-      add_ace
+
+
+  def report_values(hand)
+    @total = @hand.get_total(hand)
+    last_card = hand.last
+    other_cards = hand.reverse.drop(1).reverse.map { |card| card.rank  }.join(", a ")
+    if hand === @hand.player_cards
+      speak_slower
+      puts "You have a #{other_cards} and a #{last_card.rank} in your hand. Your total is #{@total}."
+      speak_slower
+      puts "The dealer has a #{@hand.dealer_cards[0].rank} and one hidden card."
+      if @total === 21
+        win_or_lose
+      end
     else
-      add_ace
+      speak_slower
+      puts "The dealer has a #{other_cards} and a #{last_card.rank} in their hand. Their total is #{@total}."
     end
-  else
-    @card_values.inject(&:+)
   end
-end
 
-def report_values(hand)
-  @total = get_total(hand)
-  last_card = hand.last
-  other_cards = hand.reverse.drop(1).reverse.map { |card| card.rank  }.join(", a ")
-  if hand === @hand.player_cards
-    puts "You have a #{other_cards} and a #{last_card.rank} in your hand. Your total is #{@total}."
-    puts "The dealer has a #{@hand.dealer_cards[0].rank} and one hidden card."
-    if @total === 21
-      win_or_lose
+  def make_bet
+    speak_slower
+    puts "You have $#{@cash} and bet $10."
+  end
+
+  def play_again
+    speak_slower
+    puts "Do you want to play again?"
+    print "Please enter (y)es or (n)o: "
+    answer = gets.chomp.downcase
+    if answer[0] == "y"
+      run_game
+      # return false
+    elsif answer[0] == "n"
+      quit_game
+      # return false
+    else
+      puts "That is not a valid answer!"
+      play_again
     end
-  else
-    puts "The dealer has a #{other_cards} and a #{last_card.rank} in their hand. Their total is #{@total}."
   end
-end
 
-def make_bet
-  puts "You have $#{@cash} and bet $10."
-end
+  def quit_game
+    cash_left = @cash - 100
+    speak_slower
+    puts "Thanks for playing! You won #{@games_won} games and $#{cash_left}."
+    exit
+  end
 
-def play_again
-  puts "Do you want to play again?"
-  print "Please enter (y)es or (n)o: "
-  answer = gets.chomp.downcase
-  if answer[0] == "y"
-    run_game
-    # return false
-  elsif answer[0] == "n"
-    quit_game
-    # return false
-  else
-    puts "That is not a valid answer!"
+  def end_hand
+    hidden_card = @hand.dealer_cards.last.rank
+    # speak_slower
+    # puts "The dealer's hidden card was a #{hidden_card}"
+    speak_slower
+    puts "You now have $#{@cash}."
     play_again
   end
-end
 
-def quit_game
-  cash_left = @cash - 100
-  puts "Thanks for playing! You won #{@games_won} games and #{cash_left} dollars"
-  exit
-end
+  def win_or_lose
+    if (@total === 21)
+      @cash = @cash + 10
+      @games_won = @games_won + 1
+      puts "You win!"
+      end_hand
+    elsif @total > 21
+      @cash = @cash - 10
+      puts "You lose!"
+      end_hand
+    elsif @total < 21
+      hit_or_stand
+    end
+  end
 
-def end_hand
-  hidden_card = @hand.dealer_cards.last.rank
-  puts "The dealer's hidden card was a #{hidden_card}"
-  puts "You now have $#{@cash}."
-  play_again
-end
+  def player_stands
+    report_values(@hand.dealer_cards)
+    dealer_total = @hand.get_total(@hand.dealer_cards)
+    player_total = @hand.get_total(@hand.player_cards)
+    while dealer_total < 17
+      @hand.deal_card(@hand.dealer_cards)
+      last_card = @hand.dealer_cards.last.rank
+      speak_slower
+      puts "The dealer draws a #{last_card}."
+      dealer_total = @hand.get_total(@hand.dealer_cards)
+      speak_slower
+      puts "Their total is now #{dealer_total}."
+    end
+    if dealer_total > 21
+      speak_slower
+      puts "dealer busts!"
+    end
+    if player_total >= dealer_total || dealer_total > 21
+      @cash = @cash + 10
+      @games_won = @games_won + 1
+      speak_slower
+      puts "You win!"
+      speak_slower
+      puts "You have $#{@cash} left."
+      play_again
+    else
+      @cash = @cash - 10
+      speak_slower
+      puts "You lose."
+      speak_slower
+      puts "You have $#{@cash} left."
+      play_again
+    end
+  end
 
-def win_or_lose
-  if (@total === 21)
-    @cash = @cash + 10
-    @games_won = @games_won + 1
-    puts "You win!"
-    end_hand
-  elsif @total > 21
-    @cash = @cash - 10
-    puts "You lose!"
-    end_hand
-  elsif @total < 21
+  def player_hits
+    @hand.deal_card(@hand.player_cards)
+    last_card = @hand.player_cards.last.rank
+    speak_slower
+    puts "The dealer deals you a #{last_card}."
+    report_values(@hand.player_cards)
+    win_or_lose
+  end
+
+  def begin_game
+    puts "Hello and welcome to the game of blackjack! Let's begin."
+    run_game
+  end
+
+  def run_game
+    @hand = Hand.new
+    speak_slower
+    @hand.deal
+    make_bet
+    report_values(@hand.player_cards)
     hit_or_stand
   end
-end
-
-def player_stands
-  report_values(@hand.dealer_cards)
-  dealer_total = get_total(@hand.dealer_cards)
-  player_total = get_total(@hand.player_cards)
-  # puts "Your total is #{player_total} the dealer has #{dealer_total}."
-  while dealer_total < 17
-    @hand.deal_card(@hand.dealer_cards)
-    last_card = @hand.dealer_cards.last.rank
-    puts "The dealer draws a #{last_card}."
-    # report_values(@hand.dealer_cards)
-    dealer_total = get_total(@hand.dealer_cards)
-    puts "Their total is now #{dealer_total}."
-  end
-  if dealer_total > 21
-    puts "dealer busts!"
-  end
-  if player_total >= dealer_total || dealer_total > 21
-    @cash = @cash + 10
-    @games_won = @games_won + 1
-    puts "You win"
-    puts "You have $#{@cash} left."
-    play_again
-  else
-    @cash = @cash - 10
-    puts "You lose"
-    puts "You have $#{@cash} left."
-    play_again
-  end
-end
-
-def player_hits
-  @hand.deal_card(@hand.player_cards)
-  last_card = @hand.player_cards.last.rank
-  puts "The dealer deals you a #{last_card}"
-  report_values(@hand.player_cards)
-  win_or_lose
-end
-
-def begin_game
-  puts "Hello and welcome to the game of blackjack! Let's begin."
-  run_game
-end
-
-def run_game
-  @hand = Hand.new
-  @hand.deal
-  make_bet
-  report_values(@hand.player_cards)
-  hit_or_stand
-end
 
 end
 
--- starter_files/lib/hand.rb
+++ starter_files/lib/hand.rb
@@ -11,7 +11,7 @@ class Hand
   end
 
   def deal
-    "The dealer deals you two cards and draws two."
+    puts "The dealer deals you two cards and draws two for themselves."
     2.times{deal_card(@player_cards)}
     2.times{deal_card(@dealer_cards)}
   end
@@ -20,11 +20,35 @@ class Hand
     @card = @deck.shift
     @card_rank = @card.rank
     hand.push(@card)
-    # if hand == @player_cards
-    #   puts "The dealer deals you a #{@card.rank}."
-    # else
-    #   puts "The dealer draws a #{@card.rank}."
-    # end
+  end
+
+  def add_ace
+    if @card_values.inject(&:+) < 11
+      @card_values.inject(&:+) + 11
+    else
+      @card_values.inject(&:+) + 1
+    end
+  end
+
+  def get_total(cards)
+    @card_values = cards.map { |card| card.rank  }
+    @card_values.map! { |value| (value === :J || value === :Q || value === :K ? 10 : value) }
+  # if the hand includes an Ace
+    if @card_values.include?(:A)
+      @card_values.delete_at(@card_values.find_index(:A))
+  # if the hand includes another Ace
+      if @card_values.include?(:A)
+        while @card_values.include?(:A)
+          @card_values.delete_at(@card_values.find_index(:A))
+          @card_values.push(1)
+        end
+        add_ace
+      else
+        add_ace
+      end
+    else
+      @card_values.inject(&:+)
+    end
   end
 
 end
